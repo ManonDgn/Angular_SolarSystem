7ac532735166dc6925ead79a585bb352333a4d06
Manon Digne <manon.digne@gmail.com>
2023-01-13 18:32:02 +0100

    Init + Component Loader

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoaderComponent } from './loader/loader.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+// Route 1 = Loader + Home
+// Route 2 = 
+// Route 3 = Erreur 404
+const routes: Routes = [
+  { path: '', component: LoaderComponent},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
