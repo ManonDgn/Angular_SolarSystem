94c60a9a934b611ceda7f1abb41f583822977396
Manon Digne <manon.digne@gmail.com>
2023-02-01 15:40:16 +0100

    Finalisation compo planète

--- src/app/models/planet.model.ts
+++ src/app/models/planet.model.ts
@@ -2,8 +2,7 @@ export class PlanetModel {
     id!: number;
     name!: string;
     description!: string;
+    type!: string;
     distance!: number;
-    moons!: string;
-    satellites!: string;
     imgUrl!: string;
 }
